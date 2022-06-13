23fbdcc11cb55e38779ce7c0e0aedfba03d001b4
Wen Kokke <wenkokke@users.noreply.github.com>
2022-06-13 23:27:08 +0100

    Replace dots with dashes in qualified names

--- shoggoth-agda/Shoggoth/Agda.hs
+++ shoggoth-agda/Shoggoth/Agda.hs
@@ -415,7 +415,7 @@ newtype AgdaSoup a = AgdaSoup (State.State AgdaSoupState a)
 qualifyName :: Library -> ModuleName -> Text -> Text
 qualifyName Library {libraryName} moduleName name
   | Text.null name = name
-  | otherwise = libraryName <> ":" <> moduleName <> "." <> name
+  | otherwise = libraryName <> "-" <> Text.replace "." "-" moduleName <> "-" <> name
 
 qualifyIdSoup :: AgdaFileInfo -> Tag Text -> AgdaSoup (Tag Text)
 qualifyIdSoup AgdaFileInfo {library, moduleName} = mapIdSoup (qualifyName library moduleName)
