4681f88f924ac6720c6b1cfc3c8777b0cb47bad0
Wen Kokke <wenkokke@users.noreply.github.com>
2022-06-18 00:57:04 +0100

    Update Sass.hs

--- shoggoth-hsass/Shoggoth/CSS/Sass.hs
+++ shoggoth-hsass/Shoggoth/CSS/Sass.hs
@@ -1,21 +1,58 @@
+{-# LANGUAGE CPP #-}
+
 module Shoggoth.CSS.Sass
-  ( -- * Compile Sass with hsass
-    compileSass,
-    compileSassWith,
-    minCssImporter,
-    SassOptions (..),
+  ( compileSass,
+    compileSassWith
   )
 where
 
 import Shoggoth.Prelude
+
+#if installSass
 import Shoggoth.Prelude.ByteString qualified as BS (toText)
 import Data.Bitraversable (Bitraversable (..))
 import Data.Maybe (fromMaybe)
 import System.Directory as System (doesFileExist, makeAbsolute, getCurrentDirectory)
 import Text.Sass
 import Data.Text (Text)
+#else
+-- TODO
+#endif
 
 -- * Sass
+
+#if installSass
+-- | Compile Sass.
+compileSass :: FilePath -> Action Text
+compileSass = compileSassWith []
+
+-- | Compile Sass with options.
+compileSassWith :: [FilePath] -> FilePath -> Action Text
+compileSassWith loadPaths filePath = do
+  (css, includes) <- liftIO $ do
+    -- Compile @filePath@ from Sass/SCSS to CSS
+    resultOrError <- liftIO $ compileFile filePath (sassOptions loadPaths)
+    resultOrErrorMsg <- liftIO $ bitraverse errorMessage return resultOrError
+    result <- liftIO (liftEither id resultOrErrorMsg)
+    -- Extract generated CSS source and included files
+    css <- BS.toText (resultString result)
+    includes <- resultIncludes result
+    currentWorkingDirectory <- getCurrentDirectory
+    let relativeIncludes = makeRelative currentWorkingDirectory <$> includes
+    return (css, relativeIncludes)
+
+  -- Inform Shake of the dependencies used during compilation
+  trackRead includes
+
+  return css
+
+sassOptions :: [FilePath] -> SassOptions
+sassOptions [] = def
+sassOptions loadPaths =
+  def
+    { sassIncludePaths = Just loadPaths,
+      sassImporters = Just [minCssImporter loadPath 1 | loadPath <- loadPaths]
+    }
 
 minCssImporter :: FilePath -> Double -> SassImporter
 minCssImporter includePath priority =
@@ -31,27 +68,6 @@ minCssImporter includePath priority =
           else do
             return []
     }
-
--- | Compile Sass.
-compileSass :: FilePath -> Action Text
-compileSass = compileSassWith def
-
--- | Compile Sass with options.
-compileSassWith :: SassOptions -> FilePath -> Action Text
-compileSassWith opts filePath = do
-  (css, includes) <- liftIO $ do
-    -- Compile @filePath@ from Sass/SCSS to CSS
-    resultOrError <- liftIO $ compileFile filePath opts
-    resultOrErrorMsg <- liftIO $ bitraverse errorMessage return resultOrError
-    result <- liftIO (liftEither id resultOrErrorMsg)
-    -- Extract generated CSS source and included files
-    css <- BS.toText (resultString result)
-    includes <- resultIncludes result
-    currentWorkingDirectory <- getCurrentDirectory
-    let relativeIncludes = makeRelative currentWorkingDirectory <$> includes
-    return (css, relativeIncludes)
-
-  -- Inform Shake of the dependencies used during compilation
-  trackRead includes
-
-  return css
+#else
+-- TODO
+#endif
